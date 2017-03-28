ac080c6b9cd01089e4cc0f0f70508b770a77b303
Maxim <karamba92@mail.ru>
2017-03-28 10:13:04 +0600

    Создана модель order, прставлены связи между пользователем и заказами.

--- db/migrate/20170328041044_create_orders.rb
+++ db/migrate/20170328041044_create_orders.rb
@@ -2,7 +2,8 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       t.string :title
-      t.boolean :status
+      t.boolean :delivered
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327114237) do
+ActiveRecord::Schema.define(version: 20170328041044) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "title"
-    t.boolean  "status"
+    t.boolean  "delivered"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,3 +40,4 @@ ActiveRecord::Schema.define(version: 20170327114237) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+end
