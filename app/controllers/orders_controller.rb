970fa5155b9f04e89bceb7bae0c419d9cc0963cc
Omurbek Choibekov <omka3s@gmail.com>
2017-03-29 11:21:30 +0600

    delivered status method done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,10 @@ before_action :authenticate_user!
     @order = Order.new(orders_params)
   end
 
-  def add_order
-    @order = Order.find(params[:title])
-    @order.update()
+  def delivered
+    @order = Order.find(params[:id])
+    @order.update_attribute(:delivered,true)
+    redirect_to root_path
   end
 
   def assign_order_to_user
@@ -20,13 +21,10 @@ before_action :authenticate_user!
     @user = User.find(params[:id])
   end
 
-  def save_order_to_user
-    @user.update
-  end
-
   def update
     @order = Order.find_by(:title=>params['user']['order']['title'])
     @order.update_attribute(:user_id,params['user']['user_id'])
+    redirect_to root_path
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resource :orders
 
   get '/assign_order_to_user/:id' => 'orders#assign_order_to_user', as: 'assign'
-
+  get 'delivered/:id' => 'orders#delivered', as: 'delivered'
 
 end
